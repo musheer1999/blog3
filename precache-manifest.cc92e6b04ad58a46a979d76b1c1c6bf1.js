6cc22daf51860b7f2587980ac17817026f9ff9ba
musheer1999 <musheerahmad.710@gmail.com>
2020-10-05 00:18:16 +0530

    Updates

---
+++ precache-manifest.cc92e6b04ad58a46a979d76b1c1c6bf1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c0f048e9571b0b7215aaa1c6cca153",
+    "url": "/blog3/index.html"
+  },
+  {
+    "revision": "f7330eeb72aef766d385",
+    "url": "/blog3/static/css/2.4cb94f55.chunk.css"
+  },
+  {
+    "revision": "224ef54986eae6c05492",
+    "url": "/blog3/static/css/main.ddc1f64b.chunk.css"
+  },
+  {
+    "revision": "f7330eeb72aef766d385",
+    "url": "/blog3/static/js/2.c8c15448.chunk.js"
+  },
+  {
+    "revision": "9f53b202ce30cfd8431b0df14bf4916f",
+    "url": "/blog3/static/js/2.c8c15448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "224ef54986eae6c05492",
+    "url": "/blog3/static/js/main.2bcf37a9.chunk.js"
+  },
+  {
+    "revision": "7cbc3c3b11cdd8cfa13d",
+    "url": "/blog3/static/js/runtime-main.38946c99.js"
+  },
+  {
+    "revision": "79cc99435e67e10dc130312eabe55df8",
+    "url": "/blog3/static/media/photo.79cc9943.jpg"
+  }
+]);
